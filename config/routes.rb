0adaffb96609633c1b53ba88960cb0d9ee489b1e
Tony Gaeta <tgaeta@users.noreply.github.com>
2024-07-24 09:46:14 -0400

    (chore) Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'payroll_periods/index'
-  get 'payroll_periods/create'
-  get 'payroll_periods/update'
-  get 'payroll_periods/destroy'
-  get 'organizations/index'
-  get 'organizations/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :organizations do
+    resources :payroll_periods
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'organizations#index'
 end
