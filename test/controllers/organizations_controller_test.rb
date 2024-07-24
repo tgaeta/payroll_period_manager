37fd0627d79b1923f4d86686cd529f519ea20f1a
Tony Gaeta <tgaeta@users.noreply.github.com>
2024-07-24 18:54:26 -0400

    (test) Thoroughly test controllers.

--- test/controllers/organizations_controller_test.rb
+++ test/controllers/organizations_controller_test.rb
@@ -1,13 +1,30 @@
-require "test_helper"
+require 'test_helper'
 
 class OrganizationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get organizations_index_url
+  setup do
+    @organization = organizations(:acme)
+  end
+
+  test 'should get index' do
+    get organizations_url
     assert_response :success
   end
 
-  test "should get show" do
-    get organizations_show_url
+  test 'should get show' do
+    get organization_url(@organization)
+    assert_response :success
+  end
+
+  test 'should show current payroll period' do
+    current_period = payroll_periods(:acme_monthly)
+    current_period.update(start_date: Date.today.beginning_of_month, end_date: Date.today.end_of_month)
+
+    get organization_url(@organization)
+    assert_response :success
+  end
+
+  test 'should order payroll periods by start date descending' do
+    get organization_url(@organization)
     assert_response :success
   end
 end
--- test/controllers/payroll_periods_controller_test.rb
+++ test/controllers/payroll_periods_controller_test.rb
@@ -1,23 +1,99 @@
-require "test_helper"
+require 'test_helper'
 
 class PayrollPeriodsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get payroll_periods_index_url
+  setup do
+    @organization = organizations(:acme)
+    @payroll_period = payroll_periods(:acme_monthly)
+  end
+
+  test 'should get new' do
+    get new_organization_payroll_period_url(@organization)
     assert_response :success
   end
 
-  test "should get create" do
-    get payroll_periods_create_url
+  test 'should create payroll_period' do
+    assert_difference('PayrollPeriod.count', 1) do
+      post organization_payroll_periods_url(@organization), params: {
+        payroll_period: {
+          start_date: '2024-10-01',
+          end_date: '2025-09-30',
+          period_type: 'monthly',
+          custom_range: nil
+        }
+      }
+    end
+
+    assert_redirected_to organization_path(@organization)
+    assert_equal 'Payroll period was successfully created.', flash[:notice]
+  end
+
+  test 'should not create payroll_period with overlapping data' do
+    assert_no_difference('PayrollPeriod.count') do
+      post organization_payroll_periods_url(@organization), params: {
+        payroll_period: {
+          start_date: '2023-10-01',
+          end_date: '2024-09-30',
+          period_type: 'monthly',
+          custom_range: nil
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  # it should not have a gap between the end date of the previous period and the start date of the new period
+  test 'should not create payroll_period with a gap' do
+    assert_no_difference('PayrollPeriod.count') do
+      post organization_payroll_periods_url(@organization), params: {
+        payroll_period: {
+          start_date: '2023-10-02',
+          end_date: '2024-09-30',
+          period_type: 'monthly',
+          custom_range: nil
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'should get edit' do
+    get edit_organization_payroll_period_url(@organization, @payroll_period)
     assert_response :success
   end
 
-  test "should get update" do
-    get payroll_periods_update_url
-    assert_response :success
+  test 'should update payroll_period' do
+    patch organization_payroll_period_url(@organization, @payroll_period), params: {
+      payroll_period: {
+        start_date: @payroll_period.start_date,
+        end_date: @payroll_period.end_date,
+        period_type: @payroll_period.period_type,
+        custom_range: @payroll_period.custom_range
+      }
+    }
+
+    assert_redirected_to organization_url(@organization)
+    assert_equal 'Payroll period was successfully updated.', flash[:notice]
   end
 
-  test "should get destroy" do
-    get payroll_periods_destroy_url
-    assert_response :success
+  test 'should not update payroll_period with invalid data' do
+    patch organization_payroll_period_url(@organization, @payroll_period), params: {
+      payroll_period: {
+        start_date: nil,
+        end_date: nil,
+        period_type: ''
+      }
+    }
+    assert_response :unprocessable_entity
+  end
+
+  test 'should destroy payroll_period' do
+    assert_difference('PayrollPeriod.count', -1) do
+      delete organization_payroll_period_url(@organization, @payroll_period)
+    end
+
+    assert_redirected_to @organization
+    assert_equal 'Payroll period was successfully destroyed.', flash[:notice]
   end
 end
