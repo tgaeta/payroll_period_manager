0ad6baf1a220a682a4a5b6dd01ca71bfe3b4565f
Tony Gaeta <tgaeta@users.noreply.github.com>
2024-07-24 15:21:24 -0400

    (feat) Create flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type.to_sym
+    when :notice then 'bg-blue-100 border-blue-500 text-blue-700'
+    when :success then 'bg-green-100 border-green-500 text-green-700'
+    when :error then 'bg-red-100 border-red-500 text-red-700'
+    when :alert then 'bg-yellow-100 border-yellow-500 text-yellow-700'
+    else 'bg-gray-100 border-gray-500 text-gray-700'
+    end
+  end
 end
